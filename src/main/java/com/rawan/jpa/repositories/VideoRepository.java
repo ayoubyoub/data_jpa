e26050301b5c7b539f6c19d331a7de7390084d7f
AYOUB YOUB <sis.ayoub.youb@gmail.com>
2024-04-02 13:47:52 +0000

    added

--- src/main/java/com/rawan/jpa/repositories/VideoRepository.java
+++ src/main/java/com/rawan/jpa/repositories/VideoRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rawan.jpa.model.Video;
 
+import java.util.List;
+
 public interface VideoRepository extends JpaRepository<Video, Long> {
-  
+
 }
